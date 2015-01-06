821ff6bde07c89df25ec426546cc9f95c9f8e6c5
Yurii Lokhmatov <zaqqqqqqqq@gmail.com>
2015-01-06 03:03:27 +0200

    adding keyboard

--- em5807m/em5807m.ino
+++ em5807m/em5807m.ino
@@ -51,59 +51,128 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-double f_ini=106.2;             // Стартовая частота настройки приемника. (Изменяем на нужную).
+int scanKeypad(int delay);
+void selectRadiostationAtIndex(const short radio_index);
+short channel = 0;
 
 void setup()                    // Инициализация.
 {
-  Wire.begin();                 // Инициализация двухпроводной шины I2C.
-  setFrequency(f_ini);          // Вызываем фукнкцию загрузки частоты настройки в модуль EM5807M.
+  	Wire.begin();                 // Инициализация двухпроводной шины I2C.
 
 
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+  	// set up the LCD's number of columns and rows:
+  	lcd.begin(16, 2);
+  	// Print a message to the LCD.
+  	lcd.print("hello, world!");
+
+  	pinMode(6, OUTPUT);
+  	digitalWrite(6, HIGH);
+	for (int pin_in=7; pin_in<=10; ++pin_in) {
+   		pinMode(pin_in, INPUT);
+//  		digitalWrite(pin_in, HIGH);
+	}
+
+	selectRadiostationAtIndex(0);
 }
 
+typedef enum KeyCodes {
+	KeyCodeUp = 2,
+	KeyCodeDown = 1,
+	KeyCodeOn = 4,
+	KeyCodeOff = 8
+} KeyCodes;
+
+int key_code = 0;
 
 void loop()
 {
+	const int key = scanKeypad(200);
+	if (key != key_code) {
+		key_code = key;
+		short ch = channel;
+		//	check what keys are down
+		if (key_code & KeyCodeUp) {
+			++ch;
+		}
+		if (key_code & KeyCodeDown) {
+			--ch;
+		}
+		//	validate
+		if (ch < 0) {
+			ch = 0;
+		}
+		else if (ch >= radiostations_count) {
+			ch >= radiostations_count - 1;
+		}
+		//	select new channel if changed
+		if (ch != channel) {
+			channel = ch;
+			selectRadiostationAtIndex(channel);
+		}
+		//	display on/off
+		if (key_code & KeyCodeOn) {
+				digitalWrite(6, HIGH);
+		}
+		if (key_code & KeyCodeOff) {
+				digitalWrite(6, LOW);
+		}
+	}
+}
+
+void selectRadiostationAtIndex(const short radio_index) {
+	const Radiostation *radio = &radiostations[radio_index];
 	char print_string[17];
 	const unsigned short str_leng = 16;
 	unsigned short i;
 
-	for (short radio_index=0; radio_index < radiostations_count; ++radio_index) {
+	int frq = round(radio->f*100.0);
+	int frqH = frq/100;
+	int frqL = frq - frqH * 100;
 
-		const Radiostation *radio = &radiostations[radio_index];
-
-		int frq = round(radio->f*100.0);
-		int frqH = frq/100;
-		int frqL = frq - frqH * 100;
-
- 		i = snprintf(print_string, 16, "#%02d %02d.%02dMHz", (int)radio_index+1, frqH, frqL);
-        for (; i<str_leng; ++i) {
-        	print_string[i] = ' ';
-        }
-        print_string[str_leng] = 0;
-		lcd.setCursor(0, 0);
-		lcd.print(print_string);
-
-        for (i=0; i<str_leng && radio->name[i]; ++i) {
-        	print_string[i] = radio->name[i];
-        }
-        for (; i<str_leng; ++i) {
-        	print_string[i] = ' ';
-        }
-        print_string[str_leng] = 0;
-		// set the cursor to column 0, line 1
-		// (note: line 1 is the second row, since counting begins with 0):
-		lcd.setCursor(0, 1);
-		lcd.print(print_string);
-
-		setFrequency(radio->f);
-
-		delay(2000);
+	i = snprintf(print_string, 16, "#%02d %02d.%02dMHz", (int)radio_index, frqH, frqL);
+	for (; i<str_leng; ++i) {
+		print_string[i] = ' ';
 	}
+	print_string[str_leng] = 0;
+	lcd.setCursor(0, 0);
+	lcd.print(print_string);
+
+	for (i=0; i<str_leng && radio->name[i]; ++i) {
+		print_string[i] = radio->name[i];
+	}
+	for (; i<str_leng; ++i) {
+		print_string[i] = ' ';
+	}
+	print_string[str_leng] = 0;
+	// set the cursor to column 0, line 1
+	// (note: line 1 is the second row, since counting begins with 0):
+	lcd.setCursor(0, 1);
+	lcd.print(print_string);
+
+	setFrequency(radio->f);
+}
+
+int scanKeypad(int time) {
+	const int key_delay = 20;
+	const int loop_count = time/key_delay;
+
+	int key_value = 0;
+	for (int iloop=0; iloop<loop_count; ++iloop) {
+		for (int pin_in=7; pin_in<=10; ++pin_in) {
+			int key = digitalRead(pin_in);
+			if (0 == key) {
+				key = 1 << (pin_in - 7);
+				key_value = key_value | key;
+			}
+
+		}
+		if (key_value) {
+			delay(100);
+			break;
+		}
+		delay(key_delay);
+	}
+	return key_value;
 }
 
 
